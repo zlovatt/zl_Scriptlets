a9eb7a1b192e951d0d9bc979fe650b63452e1046
Zack Lovatt <zack@zacklovatt.com>
2022-07-05 21:32:37 -0700

    Revise 'Apply Expression to Selection' UI, resolve #3

--- Apply Expression to Selection.jsx
+++ Apply Expression to Selection.jsx
@@ -2,46 +2,85 @@
  * Prompts user for an expression, and applies it to selected properties
  *
  * @author Zack Lovatt <zack@zacklovatt.com>
- * @version 0.1.0
+ * @version 0.2.0
  */
-(function applyExpressionToSelection () {
-  var DEFAULT_EXPRESSION = "loopIn() + loopOut() - value";
-  var comp = app.project.activeItem;
+(function applyExpressionToSelection() {
+  var DEFAULT_EXPRESSION = "loopIn() + loopOut() - value;";
+  _getUserExpression(DEFAULT_EXPRESSION);
 
-  if (!(comp && comp instanceof CompItem)) {
-    alert("Open a comp!");
-    return;
+  /**
+   * Applies an expression to selected prop(s)
+   *
+   * @param {string} expression        Expression to apply
+   */
+  function applyExpression(expression) {
+    var comp = app.project.activeItem;
+
+    if (!(comp && comp instanceof CompItem)) {
+      alert("Open a comp!");
+      return;
+    }
+
+    var props = comp.selectedProperties;
+
+    if (props.length === 0) {
+      alert("Select properties to apply expression to!");
+      return;
+    }
+
+    if (!expression || expression == "") {
+      alert("Enter an expression");
+      return;
+    }
+
+    app.beginUndoGroup("Apply Expression to Selected Properties");
+
+    try {
+      for (var ii = 0, il = props.length; ii < il; ii++) {
+        var prop = props[ii];
+
+        if (!prop.canSetExpression) {
+          continue;
+        }
+
+        prop.expression = expression;
+      }
+    } catch (e) {
+      alert(e);
+    } finally {
+      app.endUndoGroup();
+    }
   }
 
-  var props = comp.selectedProperties;
+  function _getUserExpression(defaultExpression) {
+    var win = new Window("palette", "Apply Expression to Selection", undefined, {
+      resizeable: true
+    });
+    win.alignChildren = "fill";
 
-  if (props.length === 0) {
-    alert("Select properties to apply expression to!");
-    return;
-  }
+    win.add("statictext", undefined, "Expression to apply:");
 
-  var userExpression = prompt("Enter an expression!", DEFAULT_EXPRESSION);
+    var editText = win.add("edittext", undefined, defaultExpression, {
+      multiline: true
+    });
+    editText.characters = 50;
+    editText.size = [500, 200];
+    editText.alignment = "fill";
 
-  if (!userExpression) {
-    alert("Enter an expression");
-    return;
-  }
+    var btnOk = win.add("button", undefined, "Apply");
+    btnOk.onClick = function () {
+      var userExpression = editText.text;
 
-  app.beginUndoGroup("Apply Expression to Selected Properties");
-
-  try {
-    for (var ii = 0, il = props.length; ii < il; ii++) {
-      var prop = props[ii];
-
-      if (!prop.canSetExpression) {
-        continue;
+      if (userExpression == "") {
+        alert("No expression to apply!");
+        return;
       }
 
-      prop.expression = userExpression;
-    }
-  } catch (e) {
-    alert(e);
-  } finally {
-    app.endUndoGroup();
+      win.close();
+      applyExpression(userExpression);
+    };
+    btnOk.alignment = "right";
+
+    win.show();
   }
 })();
